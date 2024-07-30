0df2201ffd31394ec03a27fa61e3b65a0399fb6a
adarshjyoti <145207219+adarshjyoti@users.noreply.github.com>
2024-07-30 16:38:28 +0000

    missingnum

--- 01_arrays/ARRAYS/missing_number.java
+++ 01_arrays/ARRAYS/missing_number.java
@@ -33,7 +33,28 @@ public class missing_number {
         return -1;
     }*/
 
-    
+     /*public static int MISSINGNUMBER(int[]a,int N){
+        int sum=(N*(N+1))/2;
+        int s2=0;                                               
+        for(int i=0; i<N-1;i++){
+            s2+=a[i];
+        }
+        int missing_num=sum-s2;
+        return missing_num;
+        }
+*/
+
+public static int MISSINGNUMBER(int[]a,int N){
+    int xor1=0,xor2=0;
+    for(int i=0; i<N-1;i++){
+        xor2=xor2^a[i];//xor array element
+        xor1=xor1^(i+1);//xor upto[1,,,,N-1]
+
+    }
+    xor1=xor1^N;// xor upto [1-----N]
+    return xor1^xor2;//the missing number
+}
+
     public static void main(String[] args) {
         int N=5;
         int a[]={1,2,4,5,6};
