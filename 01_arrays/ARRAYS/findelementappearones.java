c4bc2d2450e15da3954ebc29955b376867890dd7
adarshjyoti <145207219+adarshjyoti@users.noreply.github.com>
2024-07-31 06:21:00 +0000

    dsa

---
+++ 01_arrays/ARRAYS/findelementappearones.java
@@ -0,0 +1,47 @@
+package ARRAYS;
+
+public class findelementappearones {
+    public static int getsingleElement(int []arr){
+        /*                                                                  brute force approach
+        int n= arr.length;
+        for(int i=0; i<n; i++){   //Run a loop for selecting elements:
+            int num=arr[i];
+            int cnt=0;
+
+            for(int j=0; j<n; j++){  ////find the occurrence using linear search:
+                if(arr[j]==num)
+                cnt++;
+            }
+            if(cnt==1)return num;
+        }
+        return -1;
+    
+    }
+*/
+       /*  int n=arr.length;
+            int maxi = arr[0];                                  better approach 1
+            for(int i=0; i<n; i++){
+                maxi=Math.max(maxi,arr[i]);
+            }
+            int[]hash=new int[maxi+1];
+            for(int i=0; i<n; i++){
+                hash[arr[i]]++;
+
+            }
+            for(int i=0; i<n;i++){
+                if(hash[arr[i]]==1)
+                return arr[i];
+            }
+            return -1;
+
+        }*/
+
+
+        
+    public static void main(String[] args) {
+        int[]arr={4,1,2,1,2};
+        int ans=getsingleElement(arr);
+        System.out.println("the single element is " +ans );
+    }
+    
+}
